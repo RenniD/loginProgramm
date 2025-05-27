0cc27b5f0b5b32a155b230d2bdc62fd402d0fdaa
DiegoVS <Diegoanchukov@gmail.com>
2025-05-27 18:56:06 +0200

    Update

--- loginProgramm/loginProgramm.cpp
+++ loginProgramm/loginProgramm.cpp
@@ -24,7 +24,42 @@ struct Account{
 		std::cin >> password;
 	}
 
+	bool IsValidEmail(std::string mailcheck) {
+		int at = 0;
+		int dot = 0;
+		int atPos = 0;
+		int dotPos = 0;
+
+
+		for (int index = 0; index < mailcheck.size();) {
+			if (isalpha(mailcheck[0]) || isalpha(mailcheck[index])) {
+				index++;
+			}
+			else if (isdigit(mailcheck[index])) {
+				index++;
+			}
+			else if (mailcheck[index] = '@' && at < 1) {
+				index++;
+				at++;
+				atPos = mailcheck[index];
+			}
+			else if (mailcheck[index] = '.' && dot < 1 && index > atPos && isalpha(mailcheck[index -= 1]) && isalpha(mailcheck[index += 1])) {
+				index++;
+				dot++;
+				dotPos = mailcheck[index];
+			}
+			else {
+				return false;
+			}
+
+			if (index == mailcheck.size()) {
+				return true;
+			}
+		}
+	}
+
 	void LogIn(std::string mailcheck, std::string passwordcheck) {
+		
 		if (mailcheck == mail && passwordcheck == password) {
 			std::cout << "Succesfuly loged in" << std::endl;
             isLogIn = true;
@@ -42,11 +77,18 @@ struct Account{
 	}
 
 
+	void AddBalance(int cash) {
+		if (isLogIn) {
+			balance += cash;
+		}
+	}
+
     void showInfo(){
         if(isLogIn){
-            std::cout << "Email: " << mail;
+			std::cout << "Username: " << username << std::endl;
+            std::cout << "Email: " << mail << std::endl;
+			std::cout << "Balance: " << balance << std::endl;
         }
-        //
     }
 };
 
@@ -56,4 +98,8 @@ int main(){
 	Dega.SignIn();
 
 	Dega.LogIn("Debilism@gmail.com", "Autist11");
+
+	Dega.showInfo();
+
+	Dega.AddBalance(500);
 }
